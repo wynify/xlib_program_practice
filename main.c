aa9012a8f925a0ee034e059c95f33533a9f60b95
wynify <nazarsvep12@gmail.com>
2024-09-26 13:45:06 +0300

    Fix bug in shapes -> shapes[]

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <X11/Xlib.h>
+#include <X11/keysym.h>
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
@@ -26,30 +27,30 @@ int main()
         exit(1);
     }
 
+
     screen = DefaultScreen(display);
     current_color = BlackPixel(display, screen);
 
-    window = XCreateSimpleWindow(display, RootWindow(display, screen), 10, 10, 
-                                 SCREEN_WIDTH, SCREEN_HEIGHT, 1, 
+    window = XCreateSimpleWindow(display, RootWindow(display, screen), 10, 10, 800, 600, 1,
                                  BlackPixel(display, screen), WhitePixel(display, screen));
     XMapWindow(display, window);
-    XSelectInput(display, window, ExposureMask | KeyPressMask | ButtonPressMask | PointerMotionHintMask);
+    XSelectInput(display, window, ExposureMask | KeyPressMask | ButtonPressMask | PointerMotionMask);
 
     GC gc = DefaultGC(display, screen);
 
-    while(1) {
+    while (1) {
         XNextEvent(display, &event);
 
-        if(event.type == Expose){
+        if (event.type == Expose) {
+            printf("Expose event detected\n");
             draw_shapes(display, window, gc, shapes, shape_count);
         }
 
-        if(event.type == KeyPress){
-            handle_keypress(event, display, screen, &current_color, &current_shape,
-            &shape_count, window);
+        if (event.type == KeyPress) {
+            handle_keypress(event, display, screen, &current_color, &current_shape, &shape_count, window);
         }
 
-        if(event.type == ButtonPress){
+        if (event.type == ButtonPress) {
             handle_buttonpress(event, shapes, &shape_count, current_color, current_shape);
         }
     }
@@ -57,3 +58,4 @@ int main()
     XCloseDisplay(display);
     return 0;
 }
+
--- shapes.c
+++ shapes.c
@@ -1,6 +1,8 @@
 #include "shapes.h"
 
+
 void draw_shapes(Display *display, Window window, GC gc, Shape shapes[], int shape_count) {
+    printf("Drawing shapes, count: %d\n", shape_count);
     for (int i = 0; i < shape_count; i++) {
         XSetForeground(display, gc, shapes[i].color);
         if (shapes[i].shape == 0) {  // Коло
@@ -11,31 +13,33 @@ void draw_shapes(Display *display, Window window, GC gc, Shape shapes[], int sha
     }
 }
 
-void handle_keypress(XEvent event, Display *display, int screen, unsigned long *current_color, int *current_shape, int *shape_count, Window window) {
+void handle_keypress(XEvent event, Display *display, int screen, unsigned long *current_color,
+int *current_shape, int *shape_count, Window window){
     KeySym key = XLookupKeysym(&event.xkey, 0);
 
-    if (key == XK_r) {
-        *current_color = 0xFF0000;  // Червоний
+    if(key == XK_r){
+        *current_color = 0xFF0000;
     } else if (key == XK_g) {
-        *current_color = 0x00FF00;  // Зелений
-    } else if (key == XK_b) {
-        *current_color = 0x0000FF;  // Синій
-    } else if (key == XK_k) {
-        *current_color = BlackPixel(display, screen);  // Чорний
-    } else if (key == XK_w) {
-        *current_color = WhitePixel(display, screen);  // Білий
-    } else if (key == XK_q) {
-        XClearWindow(display, window);  // Очищення вікна
-        *shape_count = 0;  // Очищаємо збережені фігури
-    } else if (key == XK_s) {
-        *current_shape = 1;  // Вибір квадрату
-    } else if (key == XK_c) {
-        *current_shape = 0;  // Вибір кола
+        *current_color = 0x00FF00;
+    } else if (key == XK_b){
+        *current_color = 0x0000FF;
+    } else if (key == XK_w){
+        *current_color = WhitePixel(display, screen);
+    } else if (key == XK_w){
+        *current_color = BlackPixel(display, screen);
+    } else if (key == XK_q){
+        XClearWindow(display, window);
+        *shape_count = 0;
+    } else if (key == XK_s){
+        *current_shape = 1;
+    } else if (key == XK_c){
+        *current_shape = 0;
     }
 }
 
-void handle_buttonpress(XEvent event, Shape shapes[], int *shape_count, unsigned long current_color, int current_shape) {
-    if (event.xbutton.button == Button1 && *shape_count < MAX_SHAPES) {
+void handler_buttonpress(XEvent event, Shape shapes[], int * shape_count, unsigned long current_color,
+int current_shape) {
+    if(event.xbutton.button == Button1 && *shape_count < MAX_SHAPES){
         shapes[*shape_count].x = event.xbutton.x;
         shapes[*shape_count].y = event.xbutton.y;
         shapes[*shape_count].color = current_color;
